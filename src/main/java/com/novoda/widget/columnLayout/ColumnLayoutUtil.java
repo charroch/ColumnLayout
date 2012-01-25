f17cad69c85a900e3e634048be0f52bb4dc7f88c
Carl-Gustaf Harroch <carl@novoda.com>
2012-01-25 20:10:34 +0000

    deleting the cached layotu

--- src/main/java/com/novoda/widget/columnLayout/ColumnLayoutUtil.java
+++
@@ -1,9 +0,0 @@
-
-package com.novoda.widget.columnLayout;
-
-public class ColumnLayoutUtil {
-
-    public static int getColumnWidth(int width, int columnCount, int gap) {
-        return (width - (columnCount * gap)) / columnCount;
-    }
-}
