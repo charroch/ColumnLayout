d6b26755645d7f43814ce1edb17fbfe9756d257a
Carl-Gustaf Harroch <carl@novoda.com>
2012-02-12 16:28:59 +0000

    uped

--- project/build.scala
+++ project/build.scala
@@ -34,30 +34,6 @@ object General {
           cp.sortWith((a, b) => !a.data.toString.contains("android"))
       }
     )
-  //  ++ Seq(apkLibTask)
-
-
-  //  val apkLib = TaskKey[Unit]("hello", "Prints 'Hello World'")
-  //
-  //  val apkLibTask = apkLib <<= (sourceDirectory in Compile, classDirectory in Compile, mainAssetsPath in Android, mainResPath in Android, manifestPath in Android) map {
-  //    (sd, classDir, assets, res, manifest) => {
-  //      sbt.IO.zip(zipdirs(classDir, assets, res) x relativeTo(sd), file("/tmp/io.zip"))
-  //    }
-  //  }
-
-  //  def zipdirs(classes: File, assets: File, res: File): Seq[(File, String)] = {
-  //    val a = Seq[(File, String)]
-  //    a :+ (((classes) ** "*.class") filter ((f: File) => !f.getName.startsWith("R"))) x relativeTo(classes) :+
-  //      ((assets) ** "*") x relativeTo(assets)
-  //  }
-
-  //      ((res) ** "*")
-
-  //  lazy val dist = task {
-  //    sbt.IO.zip(List((file(".gitignore") -> "gitignore")), file("/tmp/io.zip"))
-  //    None
-  //  }
-}
 
 object AndroidBuild extends Build {
   lazy val main = Project(
@@ -71,7 +47,8 @@ object AndroidBuild extends Build {
     file("src/it"),
     settings = General.settings ++ AndroidTest.androidSettings ++ Seq(
       useProguard in Android := false,
-      libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "1.6.1"),
+      libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "1.7.1"),
+      instrumentationRunner := "novoda.android.scala.SpecRunner",
       name := "ColumnLayoutTests"
     )
   ) dependsOn main
---
+++ src/it/src/main/scala/SpecRunner.scala
@@ -0,0 +1,12 @@
+package novoda.android.scala
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Carl-Gustaf Harroch (carl@novoda.com)
+ * Date: 12/02/12
+ * Time: 16:24
+ */
+
+class SpecRunner {
+
+}
