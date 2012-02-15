f8958170c8429d3431dc98dd67458ca28720a0e0
Carl-Gustaf Harroch <carl@novoda.com>
2012-02-15 16:58:50 +0000

    screenshot spec

--- src/it/src/main/scala/TestSomeSpecs.scala
+++ src/it/src/main/scala/TestSomeSpecs.scala
@@ -10,9 +10,16 @@ import java.util.concurrent.CountDownLatch
 import novoda.android.test.ViewMatchers
 
 import novoda.widget.{TextLayoutUtil, ActivityStub}
-import org.scalatest.matchers.{MatchResult, ShouldMatchers}
+import org.scalatest.matchers.{Matcher, ShouldMatchers}
 import org.scalatest.WordSpec
 
+
+trait Screenshot {
+  suite: org.scalatest.Suite =>
+  def t() {
+    suite.suiteName
+  }
+}
 
 // http://www.scalatest.org/scaladoc/1.7.1/#org.scalatest.WordSpec@AfterWords
 class TestSomeSpec extends ViewTestCase with WordSpec with ShouldMatchers with Screenshot {
@@ -20,6 +27,12 @@ class TestSomeSpec extends ViewTestCase with WordSpec with ShouldMatchers with S
   import ViewMatchers._
 
   "A Columnist article" should {
+
+    "take a screenshot" in withScreenshot("test") {
+      val t = new TextView(getActivity);
+      be(below(t))
+    }
+
     "have a title" in {
 
       super.setUp()
@@ -58,7 +71,7 @@ class TestSomeSpec extends ViewTestCase with WordSpec with ShouldMatchers with S
     }
   }
 
-  def withScreenshot(file: String)(f: => MatchResult) {
+  def withScreenshot[T <: View](file: String)(f: => Matcher[T]): (=> Unit) {
 
   }
 
